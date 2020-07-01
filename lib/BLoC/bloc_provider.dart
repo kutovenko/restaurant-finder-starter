7a6b4e5082a15d61b1f06ed1ebb94e0a63ab0649
Alexey Kutovenko <a.kutovenko@bsl.dev>
2020-07-01 19:19:21 +0300

    BLoc provider added

---
+++ lib/BLoC/bloc_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class BlocProvider extends StatefulWidget {
+  @override
+  _BlocProviderState createState() => _BlocProviderState();
+}
+
+class _BlocProviderState extends State<BlocProvider> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Text'),
+    );
+  }
+}
